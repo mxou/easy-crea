6f91b2905a2e756aaf53d496f589a9ed018de050
mxou <maximilien.dlrm@gmail.com>
2024-10-18 01:46:56 +0200

    ajout bdd pour l'école

--- config/sae_db.sql
+++ config/sae_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 14 oct. 2024 à 10:46
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
+-- Généré le : ven. 18 oct. 2024 à 01:46
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,11 +42,27 @@ CREATE TABLE `administrateur` (
 CREATE TABLE `carte` (
   `id_carte` int(11) NOT NULL,
   `texte_carte` varchar(280) NOT NULL,
-  `valeurs_choix1` int(11) NOT NULL,
-  `valeurs_choix2` int(11) NOT NULL,
   `date_soumission` date NOT NULL,
-  `ordre_soumission` int(11) NOT NULL
+  `ordre_soumission` int(11) NOT NULL,
+  `choix1_population` int(11) NOT NULL,
+  `choix1_finances` int(11) NOT NULL,
+  `choix2_population` int(11) NOT NULL,
+  `choix2_finances` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Déchargement des données de la table `carte`
+--
+
+INSERT INTO `carte` (`id_carte`, `texte_carte`, `date_soumission`, `ordre_soumission`, `choix1_population`, `choix1_finances`, `choix2_population`, `choix2_finances`) VALUES
+(1, 'zadiuazdoiauhoih', '2024-10-14', 1, -4, 2, 2, -1),
+(2, 'Une paysanne addict au fentanyl propose un coup d\'etat contre le menuisier', '2024-10-14', 2, -2, 2, -1, 2),
+(3, 'Test creer une carte et tout la genre wow', '2024-10-14', 3, -100, 200, 200, -100),
+(4, 'Lil young pandillas, empereur du venezuela viens proposer une alliance', '2024-10-14', 4, 8, 2, 1, 0),
+(5, 'La météo est clémente, les récoltes sont très bonne mais il faut plus de gueux pour les récolter', '2024-10-14', 5, -2, 5, 1, 1),
+(6, 'Le japon souhaite se venger de pearl harbor, on est carrément pas concernés mais askip peut importe le choix on va mourir', '2024-10-15', 6, -100, -100, -99, -99),
+(7, 'Un homme se prétends ancien aventurier, il a mis fin a sa carrière suite a une flèche dans le genou. Il se dit désormais bac +4 spé trésorier', '2024-10-15', 7, 1, 5, 0, 1),
+(8, 'HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', '2024-10-17', 8, -5, 2, 4, -3);
 
 -- --------------------------------------------------------
 
@@ -70,8 +86,17 @@ CREATE TABLE `createur` (
   `ad_mail_createur` varchar(255) NOT NULL,
   `mdp_createur` varchar(255) NOT NULL,
   `genre` enum('Homme','Femme','Autre') NOT NULL,
-  `ddn` date NOT NULL
+  `ddn` date NOT NULL,
+  `role_createur` enum('utilisateur','admin') DEFAULT 'utilisateur'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Déchargement des données de la table `createur`
+--
+
+INSERT INTO `createur` (`id_createur`, `nom_createur`, `ad_mail_createur`, `mdp_createur`, `genre`, `ddn`, `role_createur`) VALUES
+(1, 'José', 'jose@gmail.com', '$2y$10$3S48KL16AAlEVGGWC7v6sOz3wPhHfmtE4sz6zcL9lydsTWaRsJBTu', 'Homme', '1980-02-14', 'utilisateur'),
+(2, 'admin', 'admin@admin.com', '$2y$10$Rvc/CWyTvVSp37H71HOL6.7dI81CY3dUKoKH2UHknY2L614Lbc9Hm', 'Homme', '1970-07-26', 'admin');
 
 -- --------------------------------------------------------
 
@@ -87,6 +112,13 @@ CREATE TABLE `deck` (
   `nb_cartes` int(11) NOT NULL,
   `nb_jaime` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Déchargement des données de la table `deck`
+--
+
+INSERT INTO `deck` (`id_deck`, `titre_deck`, `date_debut_deck`, `date_fin_deck`, `nb_cartes`, `nb_jaime`) VALUES
+(1, 'mort infernale', '2024-10-16', '2024-10-26', 10, 0);
 
 --
 -- Index pour les tables déchargées
@@ -139,19 +171,19 @@ ALTER TABLE `administrateur`
 -- AUTO_INCREMENT pour la table `carte`
 --
 ALTER TABLE `carte`
-  MODIFY `id_carte` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_carte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `createur`
 --
 ALTER TABLE `createur`
-  MODIFY `id_createur` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_createur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `deck`
 --
 ALTER TABLE `deck`
-  MODIFY `id_deck` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_deck` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
